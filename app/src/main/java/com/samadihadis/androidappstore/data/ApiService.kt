5d955ae81e0504b46550df0de3ddae03000a258c
h.samadi <samadi.hadis@gmail.com>
2024-04-14 18:29:35 +0330

    Small retrofit refactor

--- app/src/main/java/com/samadihadis/androidappstore/data/ApiService.kt
+++ app/src/main/java/com/samadihadis/androidappstore/data/ApiService.kt
@@ -2,11 +2,17 @@ package com.samadihadis.androidappstore.data
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiService {
 
-    @GET("all")
-    fun allApps(): Call<AppListResponseModel>
-
+    @GET("v3.0/android/apps/top_google_charts.json")
+    fun topGoogleAppCharts(
+        @Query("list_name") listName: String,
+        @Query("cat_key") catKey: String,
+        @Query("country") country: String,
+        @Query("limit") limit: String,
+        @Query("access_token") accessToken: String,
+    ): Call<AppListResponseModel>
 
 }
--- app/src/main/java/com/samadihadis/androidappstore/util/RetrofitClient.kt
+++ app/src/main/java/com/samadihadis/androidappstore/util/RetrofitClient.kt
@@ -8,8 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 
-    private const val BASE_URL =
-        "https://data.42matters.com/api/v3.0/android/apps/top_google_charts.json?"
+    private const val BASE_URL = "https://data.42matters.com/api/"
 
     private val logging = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BODY
@@ -18,9 +17,11 @@ object RetrofitClient {
         addInterceptor(logging)
         build()
     }
-    private val retrofit = Retrofit.Builder().baseUrl(BASE_URL)
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
-        .client(client).build()
+        .client(client)
+        .build()
 
     val apiService: ApiService = retrofit.create(ApiService::class.java)
 }
