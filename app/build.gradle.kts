b3619d6002cfcbcc4e6f41f3966090701b5b9ba8
h.samadi <samadi.hadis@gmail.com>
2024-04-15 08:41:49 +0330

    Implementation Glide

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
 
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
     implementation("com.google.code.gson:gson:2.10.1")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
 
     implementation("com.google.android.material:material:1.11.0")
 
