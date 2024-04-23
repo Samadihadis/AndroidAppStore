aac106f857fe70d2904ebe10c2ab826fc23e58f5
h.samadi <samadi.hadis@gmail.com>
2024-04-23 18:24:03 +0330

    Add visibility state

--- app/src/main/java/com/samadihadis/androidappstore/util/View.kt
+++ app/src/main/java/com/samadihadis/androidappstore/util/View.kt
@@ -1,8 +1,21 @@
 package com.samadihadis.androidappstore.util
 
+import android.view.View
 import java.text.DecimalFormat
 
 fun Float.formatNumber(): String {
     val num = DecimalFormat("##.#")
     return num.format(this)
+}
+
+fun View.visible() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.gone() {
+    this.visibility = View.GONE
+}
+
+fun View.invisible() {
+    this.visibility = View.INVISIBLE
 }
