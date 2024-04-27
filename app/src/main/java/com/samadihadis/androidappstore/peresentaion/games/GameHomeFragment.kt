05fa6e7daae8a4ae4fb087ca7fb27f0ecec0ced7
h.samadi <samadi.hadis@gmail.com>
2024-04-27 16:11:58 +0330

    Add action bar and set on click listener in game home page

--- app/src/main/java/com/samadihadis/androidappstore/peresentaion/games/GameHomeFragment.kt
+++ app/src/main/java/com/samadihadis/androidappstore/peresentaion/games/GameHomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.samadihadis.androidappstore.data.AppInfoModel
@@ -56,6 +57,21 @@ class GameHomeFragment : Fragment() {
         getDataBannerStyle()
         getDataMediumDetailStyle()
         getDataBoxStyle()
+        setupViews()
+    }
+
+
+    private fun setupViews() = with(binding) {
+        nextMediumDetailStyleImageView.setOnClickListener {
+            findNavController().navigate(
+                GameHomeFragmentDirections.actionToAppListFragment("GAME_ACTION")
+            )
+        }
+        nextBoxStyleImageView.setOnClickListener {
+            findNavController().navigate(
+                GameHomeFragmentDirections.actionToAppListFragment("GAME_CASUAL")
+            )
+        }
     }
 
     private fun startLoadingState() = with(binding) {
