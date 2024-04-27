637a4c860cc8931d43b14b3a6c899da4210deed8
h.samadi <samadi.hadis@gmail.com>
2024-04-27 15:44:06 +0330

    Add medium detail style view holder and adapter

---
+++ app/src/main/java/com/samadihadis/androidappstore/peresentaion/adapters/mediumDetailStyle/AppMediumDetailStyleAdapter.kt
@@ -0,0 +1,45 @@
+package com.samadihadis.androidappstore.peresentaion.adapters.mediumDetailStyle
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.samadihadis.androidappstore.R
+import com.samadihadis.androidappstore.data.AppInfoModel
+import com.samadihadis.androidappstore.util.formatNumber
+
+
+class AppMediumDetailStyleAdapter : RecyclerView.Adapter<AppMediumDetailStyleViewHolder>() {
+
+    private var appList: MutableList<AppInfoModel> = mutableListOf()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AppMediumDetailStyleViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_app_medium_detail_style, parent, false)
+        return AppMediumDetailStyleViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return appList.size
+    }
+
+    fun addItemList(appModelList: List<AppInfoModel>) {
+        appList.addAll(appModelList)
+        notifyItemRangeInserted(appList.size - 1, appModelList.size)
+    }
+
+    override fun onBindViewHolder(holder: AppMediumDetailStyleViewHolder, position: Int) {
+        holder.apply {
+            titleTextView.text = appList[position].title
+            val rating = appList[position].rating
+            ratingTextView.text = rating.formatNumber() + " " + "★"
+            categoryTextView.text = appList[position].category
+            Glide.with(rootLayout.context)
+                .load(appList[position].featuredGraphic)
+                .placeholder(R.drawable.banner_image_placeholder)
+                .transform(CenterCrop(), RoundedCorners(60))
+                .into(iconImageView)
+        }
+    }
+}
---
+++ app/src/main/java/com/samadihadis/androidappstore/peresentaion/adapters/mediumDetailStyle/AppMediumDetailStyleViewHolder.kt
@@ -0,0 +1,27 @@
+package com.samadihadis.androidappstore.peresentaion.adapters.mediumDetailStyle
+
+import android.view.View
+import androidx.appcompat.widget.AppCompatImageView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.textview.MaterialTextView
+import com.samadihadis.androidappstore.R
+
+class AppMediumDetailStyleViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    val rootLayout: ConstraintLayout
+    val iconImageView: AppCompatImageView
+    val titleTextView: MaterialTextView
+    val categoryTextView: MaterialTextView
+    val ratingTextView: MaterialTextView
+
+    init {
+        view.apply {
+            rootLayout = findViewById(R.id.rootItemMediumDetailStyleLayout)
+            iconImageView = findViewById(R.id.mediumDetailStyleImageView)
+            titleTextView = findViewById(R.id.mediumDetailStyleTitleTextView)
+            categoryTextView = findViewById(R.id.mediumDetailStyleCategoryTextView)
+            ratingTextView = findViewById(R.id.mediumDetailStyleRatingTextView)
+        }
+    }
+}
