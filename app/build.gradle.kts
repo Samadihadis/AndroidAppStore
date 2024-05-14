dd6a554fe04f95fd77e345154ca4223b48e47e91
h.samadi <samadi.hadis@gmail.com>
2024-05-14 17:36:08 +0330

    Update libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
@@ -55,7 +55,9 @@ dependencies {
     implementation("com.google.code.gson:gson:2.10.1")
     implementation ("com.github.bumptech.glide:glide:4.16.0")
 
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
+
+    implementation("com.google.code.gson:gson:2.10.1")
 
     implementation ("com.facebook.shimmer:shimmer:0.5.0")
 
