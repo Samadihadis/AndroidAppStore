b069c4be5e24ae21e03c9f9da7d483d9df41a515
h.samadi <samadi.hadis@gmail.com>
2024-05-30 10:13:41 +0330

    Add about app page

--- app/src/main/java/com/samadihadis/androidappstore/peresentaion/detail/AboutAppFragment.kt
+++ app/src/main/java/com/samadihadis/androidappstore/peresentaion/detail/AboutAppFragment.kt
@@ -5,12 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.samadihadis.androidappstore.R
 import com.samadihadis.androidappstore.databinding.FragmentAboutAppBinding
 
 
 class AboutAppFragment : Fragment() {
 
-    private lateinit var binding : FragmentAboutAppBinding
+    private lateinit var binding: FragmentAboutAppBinding
+    private val args by navArgs<AboutAppFragmentArgs>()
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -21,5 +27,17 @@ class AboutAppFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            Glide.with(requireContext())
+                .load(args.appInfoModel.icon72)
+                .placeholder(R.drawable.banner_image_placeholder)
+                .into(applicationImageView)
+            titleValueTextView.text = args.appInfoModel.title
+            aboutValueTextView.text = args.appInfoModel.description
+            aboutNextImageView.setOnClickListener {
+                findNavController().navigate(AboutAppFragmentDirections.actionToDetailFragment(args.appInfoModel))
+            }
+        }
     }
 }
