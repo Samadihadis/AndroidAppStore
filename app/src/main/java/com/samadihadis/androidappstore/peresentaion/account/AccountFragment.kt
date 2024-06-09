30605a2640c797a8b0898a65e138e09dff7a53db
h.samadi <samadi.hadis@gmail.com>
2024-06-09 19:51:20 +0330

    Add theme changer to account fragment

--- app/src/main/java/com/samadihadis/androidappstore/peresentaion/account/AccountFragment.kt
+++ app/src/main/java/com/samadihadis/androidappstore/peresentaion/account/AccountFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import com.samadihadis.androidappstore.databinding.FragmentAccountBinding
 
@@ -20,5 +21,14 @@ class AccountFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.themeSwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
     }
+
 }
