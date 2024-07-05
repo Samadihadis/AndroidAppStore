62886bf8e8f7b52585cd95867c802635c3ed0c51
h.samadi <samadi.hadis@gmail.com>
2024-07-05 12:00:12 +0330

    Fix navigating to app detail page

--- app/src/main/java/com/samadihadis/androidappstore/peresentaion/list/AppListFragment.kt
+++ app/src/main/java/com/samadihadis/androidappstore/peresentaion/list/AppListFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.GridLayoutManager
 import com.google.gson.Gson
@@ -47,6 +48,11 @@ class AppListFragment : Fragment() {
         with(binding.gridStyleRecyclerView) {
             layoutManager = GridLayoutManager(requireContext(), 3)
             adapter = appGridStyleAdapter
+        }
+        appGridStyleAdapter.onItemGridStyleClickListener {
+            findNavController().navigate(
+                AppListFragmentDirections.actionToDetailFragment(it)
+            )
         }
     }
 
